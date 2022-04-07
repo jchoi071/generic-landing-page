9357536ea1d593e320a7f20777105c42120b4333
jchoi071 <jchoi071@ucr.edu>
2022-04-07 00:45:09 -0700

    Center text

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-		<h1>Great things are coming soon!</h1>
-		<p>Please be patient...</p>
+		<div>
+			<h1>Great things are coming soon!</h1>
+			<p>Please be patient...</p>
+		</div>
     </div>
   );
 }
